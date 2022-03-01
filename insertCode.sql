88b1e9e1abe3d298782e3ee806bda26207f8a513
Halldor-sys <halldor.hrafn.reynisson@gmail.com>
2022-03-01 22:22:18 +0000

    New insert shit

---
+++ insertCode.sql
@@ -0,0 +1,8 @@
+INSERT INTO ingredients (name)
+VALUES ('cheese'), ('extra cheese'), ('ham'), ('extra ham'),
+('pepperoni'), ('extra pepperoni'), ('red onion'), ('extra red onion'),
+('mozzarella'), ('extra mozzarella'), ('bacon bits'), ('extra bacon bits'),
+('Jalapeno'), ('pepper cheese'), ('BBQ sauce')
+
+INSERT INTO pizzas (name, price)
+VALUES ('Pepperoni', 10), ('Margarita', 7), ('BBQ king', 12)
