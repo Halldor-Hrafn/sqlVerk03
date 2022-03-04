37e164ccf7665a6e2d06245f6e8855c690c2c461
Halldor <halldor.hrafn.reynisson@gmail.com>
2022-03-04 09:52:01 +0000

    Some ingredients and condiments added

--- insertCode.sql
+++ insertCode.sql
@@ -1,8 +1,28 @@
 INSERT INTO ingredients (name)
-VALUES ('cheese'), ('extra cheese'), ('ham'), ('extra ham'),
-('pepperoni'), ('extra pepperoni'), ('red onion'), ('extra red onion'),
-('mozzarella'), ('extra mozzarella'), ('bacon bits'), ('extra bacon bits'),
-('Jalapeno'), ('pepper cheese'), ('BBQ sauce')
+VALUES ('red onion'), ('extra red onion'), ('green pepper'), ('extra green pepper'),
+('chili'), ('extra chili'), ('jalapeno'), ('pineapple'), ('extra pineapple'),
+('bell peppers'), ('extra bell peppers'), ('gren olives'), ('extra green olives'),
+('black olives'), ('extra black olives')
+
+INSERT INTO ingredients (name)
+VALUES ('pepperoni'), ('extra pepperoni'), ('bacon bits'), ('extra bacon bits'),
+('pulled pork'), ('extra pulled pork'), ('ham'), ('extra ham')
+
+INSERT INTO ingredients (name)
+VALUES ('cheese'), ('extra cheese'), ('cheddar cheese'), ('extra cheddar cheese'),
+('cream cheese'), ('extra cream cheese'), ('mozzarella'), ('extra mozzarella')
+
+INSERT INTO ingredients (name)
+VALUES ('tomato sauce'), ('extra tomato sauce'), ('garlic oil'), ('extra garlic oil'),
+('bbq sauce'), ('extra bbq sauce')
 
 INSERT INTO pizzas (name, price)
-VALUES ('Pepperoni', 10), ('Margarita', 7), ('BBQ king', 12)
+VALUES ('Pepperoni', 10), ('Margarita', 7), ('BBQ king', 12),
+('Bistro', 10), ('T-Rex', 12), ('Meat & Cheese', 10), 
+('Nice & Cheesy', 10), ('Hawaiian', 8), ('New York', 8),
+('Tokyo', 8)
+
+INSERT INTO condiments (name, price)
+VALUES ('bread sticks', 2), ('delux bread sticks', 4), ('chocolate sticks', 3),
+('meat & cheese goodies', 4), ('spicy bread sticks', 3), ('buffalo hot wings'),
+('delux bbq wings')
